ca968a28e520f5e62961e91e82c270b46194fcc4
kleberaugus <113084934+kleberaugus@users.noreply.github.com>
2025-03-06 13:12:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ function processarTexto() {
                         tempLetras = ''; // Limpar letras para o próximo conjunto
                     }
                 }
-console.log(numeros)
+alert(numeros)
                 i++;
             }
 
