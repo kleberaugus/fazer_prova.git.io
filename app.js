22d1de043f544882feef935392a517ee5d359a9c
kleberaugus <113084934+kleberaugus@users.noreply.github.com>
2025-03-08 08:31:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // Importe o PDF.js no topo do arquivo (nível superior do módulo)
 import * as pdfjsLib from './pdfjs/pdf.mjs';
 //agora 123
+console.log("123")
 // Configuração do worker (também no topo)
 pdfjsLib.GlobalWorkerOptions.workerSrc = './pdfjs/pdf.worker.mjs';
 
